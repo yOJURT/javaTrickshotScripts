4b83a20e777cf8a10d6e7e7ee8fee06749c1a017
yOJURT <111204961+yOJURT@users.noreply.github.com>
2023-11-25 02:11:26 -0500

    Create FastBallRnac.java

---
+++ FastBallRnac.java
@@ -0,0 +1,26 @@
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.concurrent.TimeUnit;
+import java.awt.Robot;
+public class Main {
+    public static void main(String[] args) {;
+        try {
+            Robot r = new Robot();
+            int mouseClick = InputEvent.BUTTON1_MASK;
+                TimeUnit.SECONDS.sleep(1);
+                r.mousePress(mouseClick);
+                r.mouseRelease(mouseClick);
+                TimeUnit.MILLISECONDS.sleep(50);
+                r.keyPress(KeyEvent.VK_R);
+                TimeUnit.MILLISECONDS.sleep(50);
+                r.keyRelease(KeyEvent.VK_R);
+                TimeUnit.MILLISECONDS.sleep(324);
+                r.keyPress(KeyEvent.VK_1);
+                r.keyRelease(KeyEvent.VK_1);
+                TimeUnit.SECONDS.sleep(1);
+                r.keyPress(KeyEvent.VK_1);
+                r.keyRelease(KeyEvent.VK_1);
+                TimeUnit.SECONDS.sleep(4);
+        }catch (Exception e){}
+    }
+}
